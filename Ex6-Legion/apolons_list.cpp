aa4d559443cc71a8d14ba08d65725667f089872e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-20 18:29:32 +0300

    add main function and add documentation in header file of apolons list

--- Ex6-Legion/apolons_list.cpp
+++ Ex6-Legion/apolons_list.cpp
@@ -7,19 +7,11 @@ Purpose: This file contain functions of apolons list
 #include "apolons_list.h"
 
 std::shared_ptr<Apolon> add_apolon_to_middle_or_end_of_list(std::shared_ptr<Apolon> &apolons_list,
-	std::shared_ptr<Apolon> &new_apolon) {
-	/**
-	* @brief  add apolon to the middle or the end of the list, such that
-	*		  the apolon will add in sorted ascending way of personal number
-	* @param  IN std::shared_ptr<Apolon> &apolons_list - the list of apolons
-	*		  IN std::shared_ptr<Apolon> &new_apolon - the new apolon
-	* @return the sorted list by personal number after add the new apolon
-	* @notes  change the apolon list such that add the new apolon to it
-	* @author  Liri
-	*/
+															std::shared_ptr<Apolon> &new_apolon) {
 	std::shared_ptr<Apolon> current_apolon = apolons_list;
 	while (nullptr != current_apolon->_next) {
 		if (current_apolon->_next->personal_number > new_apolon->personal_number) {
+			/*find the place for the new apolon*/
 			std::shared_ptr<Apolon> apolon_to_swap = current_apolon->_next;
 			current_apolon->_next = new_apolon;
 			new_apolon->_next = apolon_to_swap;
@@ -29,31 +21,23 @@ std::shared_ptr<Apolon> add_apolon_to_middle_or_end_of_list(std::shared_ptr<Apol
 	}
 	if (nullptr == current_apolon->_next)
 	{
+		/*add apolon in the end of the list*/
 		current_apolon->_next = new_apolon;
 	}
 	return apolons_list;
 }
 
 std::shared_ptr<Apolon> add_apolon_to_sort_list(std::shared_ptr<Apolon> &apolons_list,
-	std::shared_ptr<Apolon> &new_apolon) {
-	/**
-	* @brief  add apolon to the list such that the list will be sorted
-	*		  in ascending personal number
-	* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the list of apolons
-	*		  IN std::shared_ptr<Apolon> &new_apolon - the new apolon
-	*		  OUT std::shared_ptr<Apolon> the apolons list after add to it the
-	*			new apolon
-	* @return the sorted list by personal number after add the new apolon
-	* @notes  change the apolon list, such that add to it the new apolon
-	* @author Liri
-	*/
+												std::shared_ptr<Apolon> &new_apolon) {
 	std::shared_ptr<Apolon> current_apolon = apolons_list;
 	if (nullptr == apolons_list) {
+		/*the apolon list is empty -> the new apolon is the head of the list*/
 		apolons_list = new_apolon;
 		return apolons_list;
 	}
 
 	if (current_apolon->personal_number > new_apolon->personal_number) {
+		/*add apolon as the head of the list*/
 		new_apolon->_next = current_apolon;
 		apolons_list = new_apolon;
 		return new_apolon;
@@ -63,14 +47,6 @@ std::shared_ptr<Apolon> add_apolon_to_sort_list(std::shared_ptr<Apolon> &apolons
 }
 
 int length_of_list(std::shared_ptr<Apolon> apolons_list) {
-	/**
-	* @brief  go through the list of the apolons and calculate the length
-	*		  of the list
-	* @param  IN std::shared_ptr<Apolon> &apolons_list - the list of apolons
-	*		  OUT int the length of the list
-	* @return the length of the given list
-	* @author  Liri
-	*/
 	int length = 0;
 	std::shared_ptr<Apolon> current_apolon;
 	current_apolon = apolons_list;
@@ -82,31 +58,14 @@ int length_of_list(std::shared_ptr<Apolon> apolons_list) {
 }
 
 std::shared_ptr<Apolon> create_and_add_apolon_to_list(std::shared_ptr<Apolon> &apolons_list,
-	std::string first_name,
-	std::string last_name,
-	int personal_number,
-	int age,
-	const char* gender,
-	const char* is_vegetrian,
-	int cycle_number,
-	Address address) {
-	/**
-	* @brief  create new apolon and add to the list of apolons
-	* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the list of apolons
-	*		  IN std::string first_name - the first name of the new apolon
-	*		  IN std::string last_name - the last name of the new apolon
-	*		  IN int personal_number - the personal number of the new apolon
-	*		  IN int age - the age of the new apolon
-	*		  IN const char* gender - the gender of the new wapolon
-	*		  IN const char* is_vegetrian - string that says if the new apolon
-	*		  is vegetrian
-	*		  IN int cycle_number - the cycle number of the course of the new apolon
-	*		  IN Address address - the address of the new apolon
-	*		  OUT std::shared_ptr<Apolon> - the apolon list
-	* @return the sorted list by personal number after add the new apolon
-	* @notes  change the apolon list such that add to it the new apolon
-	* @author  Liri
-	*/
+														std::string first_name,
+														std::string last_name,
+														int personal_number,
+														int age,
+														const char* gender,
+														const char* is_vegetrian,
+														int cycle_number,
+														Address address) {
 	std::shared_ptr<Apolon> new_apolon(new Apolon());
 
 	new_apolon->first_name = first_name;
@@ -123,44 +82,32 @@ std::shared_ptr<Apolon> create_and_add_apolon_to_list(std::shared_ptr<Apolon> &a
 }
 
 void print_current_apolon(std::shared_ptr<Apolon> current_apolon) {
-	/**
-	* @brief  print the fields of current apolon
-	* @param  IN std::shared_ptr<Apolon> current_apolon - the current apolon
-	* @return  this function has no return value
-	* @author  Liri
-	*/
 	std::cout << NAME
-		<< current_apolon->first_name
-		<< SPACE
-		<< current_apolon->last_name
-		<< PERSONAL_NUMBER
-		<< current_apolon->personal_number
-		<< AGE
-		<< current_apolon->age
-		<< GENDER
-		<< current_apolon->gender
-		<< IS_VEGETRIAN
-		<< current_apolon->is_vegetrian
-		<< CYCLE_NUMBER
-		<< current_apolon->cycle_number
-		<< APARTMENT_NUMBER
-		<< current_apolon->address.apartment_number
-		<< STREET
-		<< current_apolon->address.street_name
-		<< POSTAL_CODE
-		<< current_apolon->address.postal_code
-		<< std::endl;
+				<< current_apolon->first_name
+				<< SPACE
+				<< current_apolon->last_name
+				<< PERSONAL_NUMBER
+				<< current_apolon->personal_number
+				<< AGE
+				<< current_apolon->age
+				<< GENDER
+				<< current_apolon->gender
+				<< IS_VEGETRIAN
+				<< current_apolon->is_vegetrian
+				<< CYCLE_NUMBER
+				<< current_apolon->cycle_number
+				<< APARTMENT_NUMBER
+				<< current_apolon->address.apartment_number
+				<< STREET
+				<< current_apolon->address.street_name
+				<< POSTAL_CODE
+				<< current_apolon->address.postal_code
+				<< std::endl;
 }
 
-void print_apolons(std::shared_ptr<Apolon> &apolon_list) {
-	/**
-	* @brief  print the fields of all the apolons that in the list
-	* @param  IN std::shared_ptr<Apolon> &apolon_list - the apolons list
-	* @return  this function has no return value
-	* @author  Liri
-	*/
+void print_apolons(std::shared_ptr<Apolon> &apolons_list) {
 	std::shared_ptr<Apolon> current_apolon;
-	current_apolon = apolon_list;
+	current_apolon = apolons_list;
 	while (nullptr != current_apolon) {
 		print_current_apolon(current_apolon);
 		current_apolon = current_apolon->_next;
@@ -168,16 +115,9 @@ void print_apolons(std::shared_ptr<Apolon> &apolon_list) {
 	std::cout << std::endl;
 }
 
-void print_apolons_from_cycle(std::shared_ptr<Apolon> &apolon_list, int cycle) {
-	/**
-	* @brief  print the apolons from the given cycle
-	* @param  IN std::shared_ptr<Apolon> &apolon_list - the apolons list
-	*		  IN int cycle - the given cycke
-	* @return  this function has no return value
-	* @author  Liri
-	*/
+void print_apolons_from_cycle(std::shared_ptr<Apolon> &apolons_list, int cycle) {
 	std::shared_ptr<Apolon> current_apolon;
-	current_apolon = apolon_list;
+	current_apolon = apolons_list;
 	while (nullptr != current_apolon) {
 		if (current_apolon->cycle_number == cycle) {
 			print_current_apolon(current_apolon);
@@ -187,25 +127,15 @@ void print_apolons_from_cycle(std::shared_ptr<Apolon> &apolon_list, int cycle) {
 	std::cout << std::endl;
 }
 
-std::shared_ptr<Apolon> find_apolon(std::shared_ptr<Apolon> &apolon_list,
+std::shared_ptr<Apolon> find_apolon(std::shared_ptr<Apolon> &apolons_list,
 	const char* first_name,
 	const char* last_name) {
-	/**
-	* @brief  find the first apolon that mach the given first and last names
-	* @param  IN std::shared_ptr<Apolon> &apolon_list - the apolons list
-	*		  IN const char* first_name - the first name of the apolon we search
-	*		  IN const char* last_name - the last name of the apolon we search
-	*		  OUT std::shared_ptr<Apolon> - the apolon that we searched
-	* @return  std::shared_ptr<Apolon> the apolon we that match to the given
-	*			first and last names
-	* @author  Liri
-	*/
 	std::shared_ptr<Apolon> current_apolon;
-	current_apolon = apolon_list;
+	current_apolon = apolons_list;
 	while (nullptr != current_apolon) {
-		/*the apolon found*/
 		if (current_apolon->first_name == first_name &&
 			current_apolon->last_name == last_name) {
+			/*the apolon found*/
 			break;
 		}
 		current_apolon = current_apolon->_next;
@@ -213,29 +143,16 @@ std::shared_ptr<Apolon> find_apolon(std::shared_ptr<Apolon> &apolon_list,
 	return current_apolon;
 }
 
-std::shared_ptr<Apolon> remove_apolon(std::shared_ptr<Apolon> &apolon_list,
-	std::string first_name,
-	std::string last_name) {
-	/**
-	* @brief  remove the first apolon that match to the given first and last names
-	* @param  INOUT std::shared_ptr<Apolon> &apolon_list - the apolons list
-	*		  IN const char* first_name - the first name of the apolon we want to remove
-	*		  IN const char* last_name - the last name of the apolon we want to remove
-	*	      OUT std::shared_ptr<Apolon> the apolon list after remove the apolon that
-	*		  match to  the first and last given names
-	* @return std::shared_ptr<Apolon> the apolon list after remove the apolon that
-	*		  match to the first and last given names
-	* @notes  change the list of apolons such that it remove the apolon with the
-	*		  given first and last names
-	* @author  Liri
-	*/
+std::shared_ptr<Apolon> remove_apolon(std::shared_ptr<Apolon> &apolons_list,
+										std::string first_name,
+										std::string last_name) {
 	std::shared_ptr<Apolon> current_apolon;
-	current_apolon = apolon_list;
-	if (apolon_list->first_name == first_name &&
-		apolon_list->last_name == last_name) {
+	current_apolon = apolons_list;
+	if (apolons_list->first_name == first_name &&
+		apolons_list->last_name == last_name) {
 		/*remove first apolon*/
 		current_apolon.reset();
-		return apolon_list->_next;
+		return apolons_list->_next;
 	}
 	while (nullptr != current_apolon->_next) {
 		/*remove apolon from the middle or end of the list*/
@@ -249,24 +166,15 @@ std::shared_ptr<Apolon> remove_apolon(std::shared_ptr<Apolon> &apolon_list,
 		}
 		current_apolon = current_apolon->_next;
 	}
-	return apolon_list;
+	return apolons_list;
 }
 
-void remove_all_apolons_from_list(std::shared_ptr<Apolon> &apolon_list) {
-	/**
-	* @brief  remove all apolons from list, reset the pointers
-	* @param  INOUT std::shared_ptr<Apolon> &apolon_list - the apolons list
-	*          after remove all the apolons from it
-	* @return this function has no return value
-	* @notes  change the list of apolons such that it remove all
-	*			the apolons from it
-	* @author  Liri
-	*/
+void remove_all_apolons_from_list(std::shared_ptr<Apolon> &apolons_list) {
 	std::shared_ptr<Apolon> current_apolon;
 
-	while (nullptr != apolon_list) {
-		current_apolon = apolon_list;
-		apolon_list = apolon_list->_next;
+	while (nullptr != apolons_list) {
+		current_apolon = apolons_list;
+		apolons_list = apolons_list->_next;
 		current_apolon.reset();
 	}
 }
--- Ex6-Legion/apolons_list.h
+++ Ex6-Legion/apolons_list.h
@@ -9,7 +9,6 @@ Purpose: This header file contain the declaration of the functions of
 
 #include "apolon.h"
 
-const int NUMBER_OF_APOLONS = 5;
 /*Gender:*/
 const char* const MALE = "MALE";
 const char* const FEMALE = "FEMALE";
@@ -30,34 +29,133 @@ const char* const POSTAL_CODE = ", postal code: ";
 
 std::shared_ptr<Apolon> add_apolon_to_middle_or_end_of_list(std::shared_ptr<Apolon> &apolons_list,
 	std::shared_ptr<Apolon> &new_apolon);
+/**
+* @brief  add apolon to the middle or the end of the list, such that
+*		  the apolon will add in sorted ascending way of personal number
+* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the list of apolons
+*		  IN std::shared_ptr<Apolon> &new_apolon - the new apolon
+*		  OUT std::shared_ptr<Apolon> the list of apolons after add the
+*			new apolon
+* @return the sorted list by personal number after add the new apolon
+* @notes  change the apolon list such that add the new apolon to it
+* @author  Liri
+*/
 
 std::shared_ptr<Apolon> add_apolon_to_sort_list(std::shared_ptr<Apolon> &apolons_list,
 	std::shared_ptr<Apolon> &new_apolon);
+/**
+* @brief  add apolon to the list such that the list will be sorted
+*		  in ascending personal number
+* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the list of apolons
+*		  IN std::shared_ptr<Apolon> &new_apolon - the new apolon
+*		  OUT std::shared_ptr<Apolon> the apolons list after add to it the
+*			new apolon
+* @return the sorted list by personal number after add the new apolon
+* @notes  change the apolon list, such that add to it the new apolon
+* @author Liri
+*/
 
 int length_of_list(std::shared_ptr<Apolon> apolons_list);
+/**
+* @brief  go through the list of the apolons and calculate the length
+*		  of the list
+* @param  IN std::shared_ptr<Apolon> &apolons_list - the list of apolons
+*		  OUT int the length of the list
+* @return the length of the given list
+* @author  Liri
+*/
 
 std::shared_ptr<Apolon> create_and_add_apolon_to_list(std::shared_ptr<Apolon> &apolons_list,
-	std::string first_name,
-	std::string last_name,
-	int personal_number,
-	int age,
-	const char* gender,
-	const char* is_vegetrian,
-	int cycle_number,
-	Address address);
+														std::string first_name,
+														std::string last_name,
+														int personal_number,
+														int age,
+														const char* gender,
+														const char* is_vegetrian,
+														int cycle_number,
+														Address address);
+/**
+* @brief  create new apolon and add to the list of apolons
+* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the list of apolons
+*		  IN std::string first_name - the first name of the new apolon
+*		  IN std::string last_name - the last name of the new apolon
+*		  IN int personal_number - the personal number of the new apolon
+*		  IN int age - the age of the new apolon
+*		  IN const char* gender - the gender of the new wapolon
+*		  IN const char* is_vegetrian - string that says if the new apolon
+*		  is vegetrian
+*		  IN int cycle_number - the cycle number of the course of the new apolon
+*		  IN Address address - the address of the new apolon
+*		  OUT std::shared_ptr<Apolon> - the apolon list
+* @return the sorted list by personal number after add the new apolon
+* @notes  change the apolon list such that add to it the new apolon
+* @author  Liri
+*/
 
 void print_current_apolon(std::shared_ptr<Apolon> current_apolon);
+/**
+* @brief  print the fields of current apolon
+* @param  IN std::shared_ptr<Apolon> current_apolon - the current apolon
+* @return  this function has no return value
+* @author  Liri
+*/
 
-void print_apolons(std::shared_ptr<Apolon> &apolon_list);
+void print_apolons(std::shared_ptr<Apolon> &apolons_list);
+/**
+* @brief  print the fields of all the apolons that in the list
+* @param  IN std::shared_ptr<Apolon> &apolons_list - the apolons list
+* @return  this function has no return value
+* @author  Liri
+*/
 
-void print_apolons_from_cycle(std::shared_ptr<Apolon> &apolon_list, int cycle);
+void print_apolons_from_cycle(std::shared_ptr<Apolon> &apolons_list, int cycle);
+/**
+* @brief  print the apolons from the given cycle
+* @param  IN std::shared_ptr<Apolon> &apolons_list - the apolons list
+*		  IN int cycle - the given cycke
+* @return  this function has no return value
+* @author  Liri
+*/
 
-std::shared_ptr<Apolon> find_apolon(std::shared_ptr<Apolon> &apolon_list,
-	const char* first_name,
-	const char* last_name);
+std::shared_ptr<Apolon> find_apolon(std::shared_ptr<Apolon> &apolons_list,
+									const char* first_name,
+									const char* last_name);
+/**
+* @brief  find the first apolon that mach the given first and last names,
+*			if none of the apolons match return nullptr
+* @param  IN std::shared_ptr<Apolon> &apolons_list - the apolons list
+*		  IN const char* first_name - the first name of the apolon we search
+*		  IN const char* last_name - the last name of the apolon we search
+*		  OUT std::shared_ptr<Apolon> - the apolon that we searched
+* @return  std::shared_ptr<Apolon> the apolon we that match to the given
+*			first and last names, if none of the apolons match return nullptr
+* @author  Liri
+*/
 
-std::shared_ptr<Apolon> remove_apolon(std::shared_ptr<Apolon> &apolon_list,
-	std::string first_name,
-	std::string last_name);
+std::shared_ptr<Apolon> remove_apolon(std::shared_ptr<Apolon> &apolons_list,
+										std::string first_name,
+										std::string last_name);
+/**
+* @brief  remove the first apolon that match to the given first and last names
+* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the apolons list
+*		  IN const char* first_name - the first name of the apolon we want to remove
+*		  IN const char* last_name - the last name of the apolon we want to remove
+*	      OUT std::shared_ptr<Apolon> the apolon list after remove the apolon that
+*		  match to  the first and last given names
+* @return std::shared_ptr<Apolon> the apolon list after remove the apolon that
+*		  match to the first and last given names
+* @notes  change the list of apolons such that it remove the apolon with the
+*		  given first and last names
+* @author  Liri
+*/
 
-void remove_all_apolons_from_list(std::shared_ptr<Apolon> &apolon_list);
+void remove_all_apolons_from_list(std::shared_ptr<Apolon> &apolons_list);
+/**
+* @brief  remove all apolons from list, reset the pointers
+* @param  INOUT std::shared_ptr<Apolon> &apolons_list - the apolons list
+*          after remove all the apolons from it
+* @return this function has no return value
+* @notes  change the list of apolons such that it remove all
+*			the apolons from it
+* @author  Liri
+*/
