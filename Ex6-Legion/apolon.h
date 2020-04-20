03c4a5e6372aa94892f3217b6afa02bf998f96b4
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-20 17:37:08 +0300

    add header file of struct apolon

---
+++ Ex6-Legion/apolon.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: apolon.h
+Author: Liri (20/4/2020)
+Purpose: This header file contain apolon struct and address struct
+\********************************************************/
+
+
