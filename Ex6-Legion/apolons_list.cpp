200c1c0bcad0544b81b61f829df93f5360c7c5fa
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-20 17:42:18 +0300

    add files of apolons list and documentation of files

---
+++ Ex6-Legion/apolons_list.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: apolons_list.cpp
+Author: Liri (20/4/2020)
+Purpose: This file contain functions of apolons list
+\********************************************************/
+
+#include "apolon_list.h"
---
+++ Ex6-Legion/apolons_list.h
@@ -0,0 +1,10 @@
+/********************************************************\
+File Name: apolons_list.h
+Author: Liri (20/4/2020)
+Purpose: This header file contain the declaration of the functions of
+		 apolons list, and defines of constants
+\********************************************************/
+
+#pragma once
+
+#include "apolon.h"
